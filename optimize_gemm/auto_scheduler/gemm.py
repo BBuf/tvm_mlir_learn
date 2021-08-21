e6aacb5eb08989eb9625b01e0c19e224c355f255
BBuf <1182563586@qq.com>
2021-08-21 22:15:49 +0800

    update auto_scheduler gemm

--- optimize_gemm/auto_scheduler/gemm.py
+++ optimize_gemm/auto_scheduler/gemm.py
@@ -4,9 +4,9 @@ from tvm import te, auto_scheduler, topi
 from tvm.topi.testing import dense
 
 # build model
-m = 24
-n = 24
-k = 64
+m = 96
+n = 96
+k = 256
 
 @auto_scheduler.register_workload
 def gemm():
@@ -29,7 +29,7 @@ print(task.compute_dag)
 log_file = "gemm.json"
 measure_ctx = auto_scheduler.LocalRPCMeasureContext(min_repeat_ms=300)
 tune_option = auto_scheduler.TuningOptions(
-    num_measure_trials=1000,  # change this to 1000 to achieve the best performance
+    num_measure_trials=10,  # change this to 1000 to achieve the best performance
     runner=measure_ctx.runner,
     measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
     verbose=2,
@@ -43,8 +43,8 @@ sch, args = task.apply_best(log_file)
 # Kill the measurement process
 del measure_ctx
 
-print("Lowered TIR:")
-print(tvm.lower(sch, args, simple_mode=True))
+# print("Lowered TIR:")
+# print(tvm.lower(sch, args, simple_mode=True))
 
 
 func = tvm.build(sch, args, target)
@@ -52,7 +52,7 @@ func = tvm.build(sch, args, target)
 # Check correctness
 input_np = np.random.uniform(size=(m, k)).astype(np.float32)
 weight_np = np.random.uniform(size=(n, k)).astype(np.float32)
-out_np = np.matmul(input_np, weight_np)
+out_np = np.matmul(input_np, weight_np.T)
 
 dev = tvm.cpu()
 input_tvm = tvm.nd.array(input_np, device=dev)
@@ -72,3 +72,32 @@ print(
 )
 
 
+comp = 2 * m * k * n
+gflops = comp / (np.median(evaluator(input_tvm, weight_tvm, out_tvm).results)) * 1e-9
+
+print('GFlops: ', gflops)
+
+print("Equivalent python schedule:")
+print(task.print_best(log_file, print_mode="schedule"))
+
+
+# def resume_search(task, log_file):
+#     print("Resume search:")
+#     cost_model = auto_scheduler.XGBModel()
+#     cost_model.update_from_file(log_file)
+#     search_policy = auto_scheduler.SketchPolicy(
+#         task, cost_model, init_search_callbacks=[auto_scheduler.PreloadMeasuredStates(log_file)]
+#     )
+#     measure_ctx = auto_scheduler.LocalRPCMeasureContext(min_repeat_ms=300)
+#     tune_option = auto_scheduler.TuningOptions(
+#         num_measure_trials=5,
+#         runner=measure_ctx.runner,
+#         measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+#     )
+#     task.tune(tune_option, search_policy=search_policy)
+
+#     # Kill the measurement process
+#     del measure_ctx
+
+
+# resume_search(task, log_file)
--- optimize_gemm/cpufp/gen.sh
+++
@@ -1,8 +0,0 @@
-gcc -pthread -O3 -c smtl.c
-as -o cpufp_kernel_x86_avx512f.o cpufp_kernel_x86_avx512f.s
-as -o cpufp_kernel_x86_avx512_vnni.o cpufp_kernel_x86_avx512_vnni.s
-as -o cpufp_kernel_x86_fma.o cpufp_kernel_x86_fma.s
-as -o cpufp_kernel_x86_avx.o cpufp_kernel_x86_avx.s
-as -o cpufp_kernel_x86_sse.o cpufp_kernel_x86_sse.s
-gcc -pthread -D_AVX512F_ -D_AVX512_VNNI_ -D_FMA_ -D_AVX_ -D_SSE_ -c cpufp_x86.c
-gcc -pthread -lrt -o cpufp smtl.o cpufp_x86.o cpufp_kernel_x86_avx512f.o cpufp_kernel_x86_avx512_vnni.o cpufp_kernel_x86_fma.o cpufp_kernel_x86_avx.o cpufp_kernel_x86_sse.o
